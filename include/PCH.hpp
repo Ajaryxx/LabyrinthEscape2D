903cc6e01e09e1ae0efab7771da5c4feeb6a3459
Ajaryxx <impressum15@gmail.com>
2026-03-29 10:31:07 +0200

    project setup

---
+++ include/PCH.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <TGUI/TGUI.hpp>
+
+#include <iostream>
+#include <print>
+#include <format>
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include <vector>
+
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "PCH.hpp"
+
+int main(int argc, char* argv[])
+{
+	std::println("Hello World");
+	return EXIT_SUCCESS;
+}
